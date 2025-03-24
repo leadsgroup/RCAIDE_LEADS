2e339b9d3cf922792bf88517895ff1b68a951794
Matteo Guidotti <matteog2@illinois.edu>
2025-03-24 11:15:53 -0500

    Update evaluate_CRN_emission_indices.py

--- RCAIDE/Library/Methods/Emissions/Chemical_Reactor_Network_Method/evaluate_CRN_emission_indices.py
+++ RCAIDE/Library/Methods/Emissions/Chemical_Reactor_Network_Method/evaluate_CRN_emission_indices.py
@@ -104,8 +104,8 @@ def evaluate_CRN_emission_indices_no_surrogate(segment,settings,vehicle):
     CO2_total = 0 * state.ones_row(1)  
     CO_total  = 0 * state.ones_row(1) 
     H2O_total = 0 * state.ones_row(1) 
-    NO_total  = 0 * state.ones_row(1) 
-    NO2_total = 0 * state.ones_row(1) 
+    NOx_total  = 0 * state.ones_row(1) 
+    soot_total = 0 * state.ones_row(1) 
 
     for network in vehicle.networks:  
         for fuel_line in network.fuel_lines:
@@ -129,53 +129,57 @@ def evaluate_CRN_emission_indices_no_surrogate(segment,settings,vehicle):
                             P    = combustor_conditions.inputs.stagnation_pressure 
                             mdot = propulsor_conditions.core_mass_flow_rate 
                             FAR  = combustor_conditions.outputs.fuel_to_air_ratio 
+                            mdot_air_TakeOff = propulsor_conditions.core_mass_flow_rate_take_off
+                            FAR_TakeOff      = combustor_conditions.outputs.fuel_to_air_ratio_take_off
 
-                            EI_CO2_comb   = 0 * state.ones_row(1)   
-                            EI_CO_comb    = 0 * state.ones_row(1)  
-                            EI_H2O_comb   = 0 * state.ones_row(1)  
-                            EI_NO_comb    = 0 * state.ones_row(1)  
-                            EI_NO2_comb   = 0 * state.ones_row(1)                              
+                            EI_CO2_comb    = 0 * state.ones_row(1)   
+                            EI_CO_comb     = 0 * state.ones_row(1)  
+                            EI_H2O_comb    = 0 * state.ones_row(1)  
+                            EI_NOx_comb    = 0 * state.ones_row(1)  
+                            EI_soot_comb   = 0 * state.ones_row(1)            
+
                             if network.identical_propulsors == True and p_i != 0:
                                 EI_CO2_comb = EI_CO2_prev
                                 EI_CO_comb  = EI_CO_prev
                                 EI_H2O_comb = EI_H2O_prev
-                                EI_NO_comb  = EI_NO_prev
-                                EI_NO2_comb = EI_NO2_prev 
+                                EI_NOx_comb  = EI_NOx_prev
+                                EI_soot_comb = EI_soot_prev 
                                 
                             else:     
                                 for t_idx in range(n_cp):
                                     # Call cantera 
-                                    results = evaluate_cantera(combustor,T[t_idx,0],P[t_idx,0],mdot[t_idx,0],FAR[t_idx,0])
+                                    results = evaluate_cantera(combustor,T[t_idx,0],P[t_idx,0],mdot[t_idx,0],FAR[t_idx,0],mdot_air_TakeOff[t_idx,0],FAR_TakeOff[t_idx,0])
                                     
                                     EI_CO2_comb[t_idx,0] = results.EI_CO2
                                     EI_CO_comb[t_idx,0]  = results.EI_CO 
                                     EI_H2O_comb[t_idx,0] = results.EI_H2O
-                                    EI_NO_comb[t_idx,0]  = results.EI_NO 
-                                    EI_NO2_comb[t_idx,0] = results.EI_NO2
+                                    EI_NOx_comb[t_idx,0]  = results.EI_NOx 
+                                    EI_soot_comb[t_idx,0] = results.EI_soot
                                     
                                     EI_CO2_prev = EI_CO2_comb 
                                     EI_CO_prev  =  EI_CO_comb  
                                     EI_H2O_prev = EI_H2O_comb 
-                                    EI_NO_prev  =  EI_NO_comb  
-                                    EI_NO2_prev = EI_NO2_comb 
+                                    EI_NOx_prev  =  EI_NOx_comb  
+                                    EI_soot_prev = EI_soot_comb 
                                 
                             CO2_total  += np.dot(I,mdot*EI_CO2_comb)
                             CO_total   += np.dot(I,mdot *EI_CO_comb )
                             H2O_total  += np.dot(I,mdot*EI_H2O_comb)
-                            NO_total   += np.dot(I,mdot *EI_NO_comb ) 
-                            NO2_total  += np.dot(I,mdot *EI_NO2_comb)
+                            NOx_total   += np.dot(I,mdot *EI_NOx_comb ) 
+                            Soot_total  += np.dot(I,mdot *EI_soot_comb)
 
     emissions                 = Data()
     emissions.total           = Data()
     emissions.index           = Data() 
     emissions.total.CO2       = CO2_total  * combustor.fuel_data.global_warming_potential_100.CO2 
     emissions.total.H2O       = H2O_total  * combustor.fuel_data.global_warming_potential_100.H2O  
-    emissions.total.NOx       = (NO_total + NO2_total) * combustor.fuel_data.global_warming_potential_100.NOx 
+    emissions.total.NOx       = NOx_total * combustor.fuel_data.global_warming_potential_100.NOx 
+    emissions.total.Soot      = Soot_total * combustor.fuel_data.global_warming_potential_100.Soot
     emissions.index.CO2       = EI_CO2_comb
     emissions.index.CO        = EI_CO_comb 
     emissions.index.H2O       = EI_H2O_comb
-    emissions.index.NO        = EI_NO_comb 
-    emissions.index.NO2       = EI_NO2_comb 
+    emissions.index.NOx       = EI_NOx_comb 
+    emissions.index.Soot      = EI_soot_comb 
     
     state.conditions.emissions =  emissions
     return    
@@ -276,8 +280,8 @@ def evaluate_CRN_emission_indices_surrogate(segment,settings,vehicle):
     CO2_total = 0 * segment.state.ones_row(1)  
     CO_total  = 0 * segment.state.ones_row(1) 
     H2O_total = 0 * segment.state.ones_row(1) 
-    NO_total  = 0 * segment.state.ones_row(1) 
-    NO2_total = 0 * segment.state.ones_row(1) 
+    NOx_total  = 0 * segment.state.ones_row(1) 
+    Soot_total = 0 * segment.state.ones_row(1) 
 
     for network in vehicle.networks:    
         for propulsor in network.propulsors:
@@ -297,32 +301,35 @@ def evaluate_CRN_emission_indices_surrogate(segment,settings,vehicle):
                     P    = combustor_conditions.inputs.stagnation_pressure 
                     mdot = propulsor_conditions.core_mass_flow_rate 
                     FAR  = combustor_conditions.outputs.fuel_to_air_ratio 
+                    mdot_air_TakeOff = propulsor_conditions.core_mass_flow_rate_take_off
+                    FAR_TakeOff      = combustor_conditions.outputs.fuel_to_air_ratio_take_off
                     
                     pts = np.hstack((T,P,mdot,FAR )) 
 
                     EI_CO2_comb  = np.atleast_2d(surrogates.EI_CO2(pts)).T
                     EI_CO_comb   = np.atleast_2d(surrogates.EI_CO(pts)).T 
                     EI_H2O_comb  = np.atleast_2d(surrogates.EI_H2O(pts)).T 
-                    EI_NO_comb   = np.atleast_2d(surrogates.EI_NO(pts)).T 
-                    EI_NO2_comb  = np.atleast_2d(surrogates.EI_NO2(pts)).T        
+                    EI_NOx_comb  = np.atleast_2d(surrogates.EI_NOx(pts)).T 
+                    EI_soot_comb = np.atleast_2d(surrogates.EI_soot(pts)).T        
                           
                     CO2_total += np.dot(I,mdot*EI_CO2_comb)
                     CO_total  += np.dot(I,mdot *EI_CO_comb )
                     H2O_total += np.dot(I,mdot*EI_H2O_comb)
-                    NO_total  += np.dot(I,mdot *EI_NO_comb ) 
-                    NO2_total += np.dot(I,mdot *EI_NO2_comb)
+                    NOx_total  += np.dot(I,mdot *EI_NOx_comb ) 
+                    Soot_total += np.dot(I,mdot *EI_soot_comb)
 
     emissions                 = Data()
     emissions.total           = Data()
     emissions.index           = Data() 
     emissions.total.CO2       = CO2_total * combustor.fuel_data.global_warming_potential_100.CO2 
     emissions.total.H2O       = H2O_total * combustor.fuel_data.global_warming_potential_100.H2O  
-    emissions.total.NOx       = (NO_total + NO2_total) * combustor.fuel_data.global_warming_potential_100.NOx 
+    emissions.total.NOx       = NOx_total * combustor.fuel_data.global_warming_potential_100.NOx 
+    emissions.total.Soot      = Soot_total * combustor.fuel_data.global_warming_potential_100.Soot
     emissions.index.CO2       = EI_CO2_comb
     emissions.index.CO        = EI_CO_comb 
     emissions.index.H2O       = EI_H2O_comb
-    emissions.index.NO        = EI_NO_comb 
-    emissions.index.NO2       = EI_NO2_comb 
+    emissions.index.NOx       = EI_NOx_comb 
+    emissions.index.Soot      = EI_soot_comb 
  
     segment.state.conditions.emissions =  emissions
     return   
