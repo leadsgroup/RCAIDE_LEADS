df1657b7e3f47214862001e29cfe8f6359f8fdc6
Matteo Guidotti <matteog2@illinois.edu>
2025-03-24 16:39:45 -0500

    Update emissions_test.py

--- Regressions/Tests/analysis_emissions/emissions_test.py
+++ Regressions/Tests/analysis_emissions/emissions_test.py
@@ -27,9 +27,8 @@ from Boeing_737    import configs_setup as configs_setup
 # ----------------------------------------------------------------------------------------------------------------------
 
 def main():
-    # emissions_methods = ['Emission_Index_Correlation_Method', 'Emission_Index_CRN_Method']
+    emissions_methods = ['Emission_Index_Correlation_Method', 'Emission_Index_CRN_Method']
     use_surrogate     = [True, False]
-    emissions_methods = ['Emission_Index_CRN_Method']
     
     cantera_installation = False 
     try: 
@@ -80,6 +79,9 @@ def main():
                     print('EI H2O Error: ',diff_EI_H2O)
                     assert (diff_EI_H2O/true_EI_H2O) < 1e-1
                 i += 1
+
+    plot_PZ_results(results)
+    plot_SZ_results(results)
              
     return 
 
@@ -208,7 +210,191 @@ def missions_setup(mission):
     missions.append(mission)
     
     # done!
-    return missions  
+    return missions 
+
+def plot_PZ_results(results):
+    """
+    Plot combustor performance results.
+
+    Parameters
+    ----------
+    phi_data : list
+        Equivalence ratios for each PSR
+    f_PSR_data : list
+        Mass flow fractions for each PSR
+    T_data : list
+        Temperature data for each PSR
+    EI_NOx_data : list
+        NOx emission index data for each PSR
+    EI_CO2_data : list
+        CO2 emission index data for each PSR
+    EI_CO_data : list
+        CO emission index data for each PSR
+    EI_H2O_data : list
+        H2O emission index data for each PSR
+    EI_soot_data : list
+        Soot emission index data for each PSR
+    """
+    
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    x_eq = [0.39775725593667544, 0.5362796833773087, 0.6715039577836412, 0.8100263852242745, 0.9485488126649076, 1.0837730870712403, 1.2222955145118735, 1.3608179419525066, 1.4960422163588394, 1.6345646437994725, 1.7730870712401061, 1.9116094986807393, 2.0468337730870716, 2.185356200527705, 2.3238786279683383, 2.4591029023746707, 2.5976253298153043, 2.7361477572559374, 2.8713720316622697, 3.0098944591029033, 3.1484168865435365]
+    y_eq = [0.01113783872272403, 0.016367987211832913, 0.023003413708761064, 0.031043781175972132, 0.040233783679679544, 0.050062640833541984, 0.060019066694913636, 0.06908133197234367, 0.07622770737438132, 0.0809469069583807, 0.08260041311172316, 0.08093275138185392, 0.07619956474009593, 0.06891146505402232, 0.05996278142634287, 0.049992368507212614, 0.04014952429559157, 0.03081762898907999, 0.022891011690387694, 0.01624159813570096, 0.010997294070065292]
+    color = 'black'
+    ax1.set_xlabel('Equivalence ratio in primary zone [-]')
+    ax1.set_ylabel('Mass flow fraction in primary zone [-]', color=color)
+    line1, = ax1.plot(results.segments.cruise.conditions.emissions.index.PZ_phi, results.segments.cruise.conditions.emissions.index.PZ_f_psr, '-', color=color, label='Mass Flow Fraction RCAIDE')
+    marker1, = ax1.plot(x_eq, y_eq, 'o', color=color, label='Mass Flow Fraction Literature')
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax2 = ax1.twinx()
+    x_temp = [0.39928559454902235, 0.5356771321231005, 0.6753350162208, 0.8116217841894036, 0.951137921173814, 1.0871535207517773, 1.2229534182008222, 1.3620319880094265, 1.497794907950657, 1.6368858035951999, 1.7726795381262754, 1.911807411278633, 2.0509414473489596, 2.1867783223058184, 2.3226336860165855, 2.461804699594726, 2.5976662262234624, 2.7368557285555104, 2.8727295810201845, 3.008609596402828, 3.147811424570814]
+    y_temp = [1663.5687732342003, 1934.9442379182153, 2176.579925650557, 2384.758364312267, 2540.8921933085494, 2585.5018587360587, 2499.9999999999995, 2392.1933085501855, 2284.386617100371, 2184.0148698884755, 2094.795539033457, 2016.7286245353157, 1942.3791821561335, 1879.1821561338286, 1827.137546468401, 1775.0929368029738, 1726.7657992565053, 1685.873605947955, 1644.9814126394049, 1607.806691449814, 1574.349442379182]
+    color = 'orange'
+    ax2.set_ylabel('Temperature [K]', color=color)
+    line2, = ax2.plot(results.segments.cruise.conditions.emissions.index.PZ_phi, results.segments.cruise.conditions.emissions.index.PZ_T, '-', color=color, label='Temperature RCAIDE')
+    marker2, = ax2.plot(x_temp, y_temp, 'o', color=color, label='Temperature Literature')
+    ax2.tick_params(axis='y', labelcolor=color)
+    lines = [line1, marker1, line2, marker2]
+    ax1.legend(lines, [l.get_label() for l in lines], loc='upper right')
+    fig.tight_layout()
+    # plt.savefig('Equivalence_ratio_temperature_primary_zone_reactor_i.png', dpi=300)
+
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    x_co = [0.3972148541114058, 0.53315649867374, 0.6724137931034483, 0.8083554376657823, 0.9476127320954907, 1.080238726790451, 1.2194960212201593, 1.3587533156498677, 1.4946949602122017, 1.6339522546419096, 1.7698938992042441, 1.909151193633952, 2.0450928381962865, 2.181034482758621, 2.320291777188329, 2.456233421750663, 2.5954907161803713, 2.7314323607427053, 2.8706896551724137, 3.006631299734748, 3.145888594164456]
+    y_co = [85, 27.500000000000227, 25.000000000000227, 52.5, 160, 460, 860.0000000000001, 1150, 1322.5, 1390, 1387.5, 1342.5, 1272.5, 1202.5, 1142.5, 1090, 1040, 995, 952.5, 912.5, 875]
+    color = 'tab:red'
+    ax1.set_xlabel('Equivalence ratio in primary zone reactor i [-]')
+    ax1.set_ylabel(r'EI CO [g/kg$_{\text{fuel}}$]', color=color)
+    line1, = ax1.plot(results.segments.cruise.conditions.emissions.index.PZ_phi, results.segments.cruise.conditions.emissions.index.PZ_EI_CO, '-', color=color, label='EI CO RCAIDE')
+    marker1, = ax1.plot(x_co, y_co, 'o', color=color, label='EI CO Literature')
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax2 = ax1.twinx()
+    x_nox = [0.39592426855198065, 0.5322344173409326, 0.671522258549804, 0.8063555197328927, 0.9412306360303175, 1.0827547362051522, 1.2203265177262388, 1.360164454723529, 1.4965762516472976, 1.6361929544688107, 1.772556916976195, 1.9089208794835797, 2.0485316030030445, 2.1848955655104287, 2.321259528017813, 2.460870251537278, 2.5972342140446627, 2.7368449375641277, 2.873208900071512, 3.0095728625788962, 3.1491835860983612]
+    y_nox = [0.11685947922671858, 1.270051589418074, 8.22419512615133, 41.21908401876067, 73.31157664628124, 32.05197687684312, 6.004367282215881, 1.098445620639609, 0.06010394418680676, -0.07600888763457192, -0.08304054684305129, -0.09007220605154487, -0.09727128571736898, -0.10430294492584835, -0.11133460413434193, -0.11853368380016605, -0.1255653430086454, -0.13276442267448374, -0.1397960818829631, -0.1468277410914567, -0.1540268207572808]
+    color = 'tab:purple'
+    ax2.set_ylabel(r'EI NO$_{x}$ [g/kg$_{\text{fuel}}$]', color=color)
+    line2, = ax2.plot(results.segments.cruise.conditions.emissions.index.PZ_phi, results.segments.cruise.conditions.emissions.index.PZ_EI_NOx, '-', color=color, label='EI NOx RCAIDE')
+    marker2, = ax2.plot(x_nox, y_nox, 'o', color=color, label='EI NOx Literature')
+    ax2.tick_params(axis='y', labelcolor=color)
+    lines = [line1, marker1, line2, marker2]
+    ax1.legend(lines, [l.get_label() for l in lines], loc='upper right')
+    fig.tight_layout()
+
+    # plt.savefig('Emission_Indices_CO_NOx.png', dpi=300)
+    
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    color = 'tab:blue'
+    ax1.set_xlabel('Equivalence ratio in primary zone reactor i [-]')
+    ax1.set_ylabel(r'EI CO$_{2}$ [g/kg$_{\text{fuel}}$]', color=color)
+    line1, = ax1.plot(results.segments.cruise.conditions.emissions.index.PZ_phi, results.segments.cruise.conditions.emissions.index.PZ_EI_CO2, '-', color=color, label='EI CO2 RCAIDE')
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax2 = ax1.twinx()
+    color = 'tab:green'
+    ax2.set_ylabel(r'EI H$_{2}$O [g/kg$_{\text{fuel}}$]', color=color)
+    line2, = ax2.plot(results.segments.cruise.conditions.emissions.index.PZ_phi, results.segments.cruise.conditions.emissions.index.PZ_EI_H2O, '-', color=color, label='EI H2O RCAIDE')
+    ax2.tick_params(axis='y', labelcolor=color)
+    lines = [line1, line2]
+    ax1.legend(lines, [l.get_label() for l in lines], loc='upper right')
+    fig.tight_layout()
+    # plt.savefig('Emission_Indices_CO2_H2O.png', dpi=300)
+
+    plt.figure(figsize=(10, 6))
+    line1, = plt.plot(results.segments.cruise.conditions.emissions.index.PZ_phi,  results.segments.cruise.conditions.emissions.index.PZ_EI_soot, '-', color=color, label='EI soot RCAIDE')
+    plt.xlabel('Equivalence ratio in primary zone reactor i [-]')
+    plt.ylabel(r'EI soot at exit of primary zone reactor i [g/kg$_{\text{fuel}}$]')
+    plt.grid(True, linestyle='--', alpha=0.7)
+    lines = [line1]
+    plt.legend(lines, [l.get_label() for l in lines], loc='upper left')
+    # plt.savefig('Emission_Indices_Soot.png', dpi=300)
+
+def plot_SZ_results(results):
+    """
+    Plot emission indices (EI) trends over the secondary zone (SZ) length for the slow mode.
+    
+    Parameters:
+    - z_percentages: List of positions as percentages downstream in the SZ [%]
+    - EI_CO2_list: List of EI values for CO2 [g/kg_fuel]
+    - EI_NOx_list: List of EI values for NOx [g/kg_fuel]
+    - EI_CO_list: List of EI values for CO [g/kg_fuel]
+    - EI_H2O_list: List of EI values for H2O [g/kg_fuel]
+    - EI_soot_list: List of EI values for soot [g/kg_fuel]
+    """
+    
+    # Plot CO2 EI
+    plt.figure(figsize=(10, 6))
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_sm_z, results.segments.cruise.conditions.emissions.index.SZ_sm_EI_CO2, 'b-', label='Slow Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_fm_z, results.segments.cruise.conditions.emissions.index.SZ_fm_EI_CO2, 'r-', label='Fast Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_joint_z, results.segments.cruise.conditions.emissions.index.SZ_joint_EI_CO2, 'g-', label='Joint Mixing')
+    plt.xlim(0, 100)
+    plt.xlabel('Percentage downstream SZ [%]')
+    plt.ylabel(r'EI CO$_2$ [g/kg$_{\text{fuel}}$]')
+    plt.legend()
+    plt.grid(True)
+
+    # Plot NOx EI
+    plt.figure(figsize=(10, 6))
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_sm_z, results.segments.cruise.conditions.emissions.index.SZ_sm_EI_NOx, 'b-', label='Slow Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_fm_z, results.segments.cruise.conditions.emissions.index.SZ_fm_EI_NOx, 'r-', label='Fast Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_joint_z, results.segments.cruise.conditions.emissions.index.SZ_joint_EI_NOx, 'g-', label='Joint Mixing')
+    plt.xlim(0, 100)
+    plt.xlabel('Percentage downstream SZ [%]')
+    plt.ylabel(r'EI NO$_x$ [g/kg$_{\text{fuel}}$]')
+    plt.legend()
+    plt.grid(True)
+
+    # Plot CO EI
+    plt.figure(figsize=(10, 6))
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_sm_z, results.segments.cruise.conditions.emissions.index.SZ_sm_EI_CO, 'b-', label='Slow Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_fm_z, results.segments.cruise.conditions.emissions.index.SZ_fm_EI_CO, 'r-', label='Fast Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_joint_z, results.segments.cruise.conditions.emissions.index.SZ_joint_EI_CO, 'g-', label='Joint Mixing')
+    plt.xlim(0, 100)
+    plt.xlabel('Percentage downstream SZ [%]')
+    plt.ylabel(r'EI CO [g/kg$_{\text{fuel}}$]')
+    plt.legend()
+    plt.grid(True)
+
+    # Plot H2O EI
+    plt.figure(figsize=(10, 6))
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_sm_z, results.segments.cruise.conditions.emissions.index.SZ_sm_EI_H2O, 'b-', label='Slow Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_fm_z, results.segments.cruise.conditions.emissions.index.SZ_fm_EI_H2O, 'r-', label='Fast Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_joint_z, results.segments.cruise.conditions.emissions.index.SZ_joint_EI_H2O, 'g-', label='Joint Mixing')
+    plt.xlim(0, 100)
+    plt.xlabel('Percentage downstream SZ [%]')
+    plt.ylabel(r'EI H$_2$O [g/kg$_{\text{fuel}}$]')
+    plt.legend()
+    plt.grid(True)
+
+    # Plot Soot EI
+    plt.figure(figsize=(10, 6))
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_sm_z, results.segments.cruise.conditions.emissions.index.SZ_sm_EI_soot, 'b-', label='Slow Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_fm_z, results.segments.cruise.conditions.emissions.index.SZ_fm_EI_soot, 'r-', label='Fast Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_joint_z, results.segments.cruise.conditions.emissions.index.SZ_joint_EI_soot, 'g-', label='Joint Mixing')
+    plt.xlim(0, 100)
+    plt.xlabel('Percentage downstream SZ [%]')
+    plt.ylabel(r'EI soot [g/kg$_{\text{fuel}}$]')
+    plt.legend()
+    plt.grid(True)
+
+    # Plot temperature
+    plt.figure(figsize=(10, 6))
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_sm_z, results.segments.cruise.conditions.emissions.index.SZ_sm_T, 'b-', label='Slow Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_fm_z, results.segments.cruise.conditions.emissions.index.SZ_fm_T, 'r-', label='Fast Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_joint_z, results.segments.cruise.conditions.emissions.index.SZ_joint_T, 'g-', label='Joint Mixing')
+    plt.xlim(0, 100)
+    plt.xlabel('Percentage downstream SZ [%]')  
+    plt.ylabel('Temperature [K]')
+    plt.legend()
+    plt.grid(True)
+
+    # Plot equivalence ratio
+    plt.figure(figsize=(10, 6))
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_sm_z, results.segments.cruise.conditions.emissions.index.SZ_sm_phi, 'b-', label='Slow Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_fm_z, results.segments.cruise.conditions.emissions.index.SZ_fm_phi, 'r-', label='Fast Mode')
+    plt.plot(results.segments.cruise.conditions.emissions.index.SZ_joint_z, results.segments.cruise.conditions.emissions.index.SZ_joint_phi, 'g-', label='Joint Mixing')
+    plt.xlim(0, 100)
+    plt.xlabel('Percentage downstream SZ [%]')  
+    plt.ylabel('Equivalence Ratio [-]')
+    plt.legend()
+    plt.grid(True)
      
 
 if __name__ == '__main__': 
