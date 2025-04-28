85b76f507d56339ba295baf1a529fdf7bb609342
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2025-04-28 09:40:22 -0500

    Update total_drag.py
    Placeholders for flap and slat drag

--- RCAIDE/Library/Methods/Aerodynamics/Common/Drag/total_drag.py
+++ RCAIDE/Library/Methods/Aerodynamics/Common/Drag/total_drag.py
@@ -40,9 +40,11 @@ def total_drag(state,settings,geometry):
     miscellaneous_drag    = drag.miscellaneous.total
     cooling_drag          = drag.cooling.total 
     spoiler_drag          = drag.spoiler.total 
+    #flap_drag             = drag.flap.total 
+    #slat_drag             = drag.slat.total 
 
     # untrimmed drag 
-    untrimmed_drag  =  parasite_total + induced_total  + compressibility_total + miscellaneous_drag + cooling_drag + spoiler_drag
+    untrimmed_drag  =  parasite_total + induced_total  + compressibility_total + miscellaneous_drag + cooling_drag + spoiler_drag # + flap_drag + slat_drag
     
     # trim correction
     corrected_aircraft_total_trim_drag = trim_correction_factor * untrimmed_drag   
