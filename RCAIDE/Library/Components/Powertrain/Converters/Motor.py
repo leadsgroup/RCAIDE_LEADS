d9e1f143c8d5ebe7046d1981c4039c89414dbf88
Matteo Guidotti <matteog2@illinois.edu>
2025-09-13 18:19:41 -0500

    Update motor

--- RCAIDE/Library/Components/Powertrain/Converters/Motor.py
+++ RCAIDE/Library/Components/Powertrain/Converters/Motor.py
@@ -93,60 +93,121 @@ class Motor(Converter):
         None
         """           
         self.tag                     = 'motor' 
-        self.motor_type              = 'DC'
+        self.type              = 'DC' # or PMSM
 
         # General attributes for DC and AC Motors
-        self.resistance              = 0.0
-        self.no_load_current         = 0.0
-        self.speed_constant          = 0.0
-        self.efficiency              = 1.0
-        self.gearbox                 = Data()
-        self.gearbox.gear_ratio      = 1.0 
-        self.design_angular_velocity = 0.0 
-        self.design_torque           = 0.0 
-        self.design_current          = 0.0
-        self.inverse_calculation     = False
-        self.interpolated_func       = None
+        # self.resistance              = 0.0
+        # self.no_load_current         = 0.0
+        # self.speed_constant          = 0.0
+        # self.efficiency              = 1.0
+        # self.gearbox                 = Data()
+        # self.gearbox.gear_ratio      = 1.0 
+        # self.design_angular_velocity = 0.0 
+        # self.design_torque           = 0.0 
+        # self.design_current          = 0.0
+        # self.inverse_calculation     = False
+        # self.interpolated_func       = None
 
         # Additional properties for AC motor parametrization
     
-        # Input data from Datasheet      
-        self.speed_constant                = 6.56                        # [rpm/V]        speed constant
-        self.stator_inner_diameter         = 0.16                        # [m]            stator inner diameter
-        self.stator_outer_diameter         = 0.348                       # [m]            stator outer diameter
+        # # Input data from Datasheet      
+        # self.speed_constant                = 6.56                        # [rpm/V]        speed constant
+        # self.stator_inner_diameter         = 0.16                        # [m]            stator inner diameter
+        # self.stator_outer_diameter         = 0.348                       # [m]            stator outer diameter
 
-        # Input data from Literature      
-        self.winding_factor                = 0.95                        # [-]            winding factor
+        # # Input data from Literature      
+        # self.winding_factor                = 0.95                        # [-]            winding factor
 
-        # Input data from Assumptions
-        self.resistance                    = 0.002                       # [Ω]            resistance
-        self.motor_stack_length            = 0.1140                      # [m]            (It should be around 0.14 m) motor stack length 
-        self.number_of_turns               = 80                          # [-]            number of turns  
-        self.length_of_path                = 0.4                         # [m]            length of the path  
-        self.mu_0                          = 1.256637061e-6              # [N/A**2]       permeability of free space
-        self.mu_r                          = 1005                        # [N/A**2]       relative permeability of the magnetic material 
-        self.thermal_conductivity          = 200                         # [W/m*K]        thermal conductivity of the magnetic material
-        self.Delta_T                       = 10                          # [K]            temperature difference between the inner and outer surfaces of the stator
-        self.characteristic_length_of_flow = 0.01                    # [m]            characteristic length of the flow
-        self.thermal_conductivity_fluid    = 0.026                      # [W/m*K]        thermal conductivity of the fluid
-        self.length_of_conductive_path     = 0.4                         # [m]            length of the conductive path  
-        self.Re_cooling_flow               = 100000                      # [-]            Reynolds number of the coolingflow
-        self.Re_airgap                     = 100000                      # [-]            Reynolds number of the flow in the airgap
-        self.Prandtl_number                = 0.708                       # [-]            Prandtl number of the flow
-        self.height_of_duct                = 0.005                       # [m]            height of the duct
-        self.width_of_duct                 = 0.005                       # [m]            width of the duct
-        self.hydraulic_diameter_of_duct    = 0.005                      # [m]            hydraulic diameter of the duct
-        self.length_of_channel             = 0.005                       # [m]            length of the channel
-        self.volume_flow_rate_of_fluid     = 0.005                       # [m**3/s]       volume flow rate of the fluid
-        self.density_of_fluid              = 1000                        # [kg/m**3]      density of the fluid
-        self.velocity_of_fluid             = 0.005                       # [m/s]          velocity of the fluid
-        self.Taylor_number                 = 20                          # [-]            Taylor number 
-        self.axial_gap_to_radius_of_rotor  = 0.01                     # [-]            ratio of the axial gap to the radius of the rotor 
-        self.inverse_calculation           = False
-        self.Conduction_laminar_flow       = True                        # [-]            True if the flow is laminar, False if the flow is turbulent
-        self.Convection_laminar_flow       = True                        # [-]            True if the flow is laminar, False if the flow is turbulent
+        # # Input data from Assumptions
+        # self.resistance                    = 0.002                       # [Ω]            resistance
+        # self.motor_stack_length            = 0.1140                      # [m]            (It should be around 0.14 m) motor stack length 
+        # self.number_of_turns               = 80                          # [-]            number of turns  
+        # self.length_of_path                = 0.4                         # [m]            length of the path  
+        # self.mu_0                          = 1.256637061e-6              # [N/A**2]       permeability of free space
+        # self.mu_r                          = 1005                        # [N/A**2]       relative permeability of the magnetic material 
+        # self.thermal_conductivity          = 200                         # [W/m*K]        thermal conductivity of the magnetic material
+        # self.Delta_T                       = 10                          # [K]            temperature difference between the inner and outer surfaces of the stator
+        # self.characteristic_length_of_flow = 0.01                    # [m]            characteristic length of the flow
+        # self.thermal_conductivity_fluid    = 0.026                      # [W/m*K]        thermal conductivity of the fluid
+        # self.length_of_conductive_path     = 0.4                         # [m]            length of the conductive path  
+        # self.Re_cooling_flow               = 100000                      # [-]            Reynolds number of the coolingflow
+        # self.Re_airgap                     = 100000                      # [-]            Reynolds number of the flow in the airgap
+        # self.Prandtl_number                = 0.708                       # [-]            Prandtl number of the flow
+        # self.height_of_duct                = 0.005                       # [m]            height of the duct
+        # self.width_of_duct                 = 0.005                       # [m]            width of the duct
+        # self.hydraulic_diameter_of_duct    = 0.005                      # [m]            hydraulic diameter of the duct
+        # self.length_of_channel             = 0.005                       # [m]            length of the channel
+        # self.volume_flow_rate_of_fluid     = 0.005                       # [m**3/s]       volume flow rate of the fluid
+        # self.density_of_fluid              = 1000                        # [kg/m**3]      density of the fluid
+        # self.velocity_of_fluid             = 0.005                       # [m/s]          velocity of the fluid
+        # self.Taylor_number                 = 20                          # [-]            Taylor number 
+        # self.axial_gap_to_radius_of_rotor  = 0.01                     # [-]            ratio of the axial gap to the radius of the rotor 
+        # self.inverse_calculation           = False
+        # self.Conduction_laminar_flow       = True                        # [-]            True if the flow is laminar, False if the flow is turbulent
+        # self.Convection_laminar_flow       = True                        # [-]            True if the flow is laminar, False if the flow is turbulent
         
+    @property
+    def motor_type(self):
+        return self._type
+
+    @motor_type.setter
+    def motor_type(self, value: str):
+        self._type = value
+
+        if value == 'DC':
+            self.resistance              = 0.0
+            self.no_load_current         = 0.0
+            self.speed_constant          = 0.0
+            self.efficiency              = 1.0
+            self.gearbox                 = Data()
+            self.gearbox.gear_ratio      = 1.0 
+            self.design_angular_velocity = 0.0 
+            self.design_torque           = 0.0 
+            self.design_current          = 0.0
+            self.inverse_calculation     = False
+            self.interpolated_func       = None
+
+        elif value == 'PMSM':
+            # Input data from Datasheet      
+            self.speed_constant                = 6.56                        # [rpm/V]        speed constant
+            self.stator_inner_diameter         = 0.16                        # [m]            stator inner diameter
+            self.stator_outer_diameter         = 0.348                       # [m]            stator outer diameter
+
+            # Input data from Literature      
+            self.winding_factor                = 0.95                        # [-]            winding factor
+
+            # Input data from Assumptions
+            self.resistance                    = 0.002                       # [Ω]            resistance
+            self.motor_stack_length            = 0.1140                      # [m]            (It should be around 0.14 m) motor stack length 
+            self.number_of_turns               = 80                          # [-]            number of turns  
+            self.length_of_path                = 0.4                         # [m]            length of the path  
+            self.mu_0                          = 1.256637061e-6              # [N/A**2]       permeability of free space
+            self.mu_r                          = 1005                        # [N/A**2]       relative permeability of the magnetic material 
+            self.thermal_conductivity          = 200                         # [W/m*K]        thermal conductivity of the magnetic material
+            self.Delta_T                       = 10                          # [K]            temperature difference between the inner and outer surfaces of the stator
+            self.characteristic_length_of_flow = 0.01                    # [m]            characteristic length of the flow
+            self.thermal_conductivity_fluid    = 0.026                      # [W/m*K]        thermal conductivity of the fluid
+            self.length_of_conductive_path     = 0.4                         # [m]            length of the conductive path  
+            self.Re_cooling_flow               = 100000                      # [-]            Reynolds number of the coolingflow
+            self.Re_airgap                     = 100000                      # [-]            Reynolds number of the flow in the airgap
+            self.Prandtl_number                = 0.708                       # [-]            Prandtl number of the flow
+            self.height_of_duct                = 0.005                       # [m]            height of the duct
+            self.width_of_duct                 = 0.005                       # [m]            width of the duct
+            self.hydraulic_diameter_of_duct    = 0.005                      # [m]            hydraulic diameter of the duct
+            self.length_of_channel             = 0.005                       # [m]            length of the channel
+            self.volume_flow_rate_of_fluid     = 0.005                       # [m**3/s]       volume flow rate of the fluid
+            self.density_of_fluid              = 1000                        # [kg/m**3]      density of the fluid
+            self.velocity_of_fluid             = 0.005                       # [m/s]          velocity of the fluid
+            self.Taylor_number                 = 20                          # [-]            Taylor number 
+            self.axial_gap_to_radius_of_rotor  = 0.01                     # [-]            ratio of the axial gap to the radius of the rotor 
+            self.inverse_calculation           = False
+            self.Conduction_laminar_flow       = True                        # [-]            True if the flow is laminar, False if the flow is turbulent
+            self.Convection_laminar_flow       = True                        # [-]            True if the flow is laminar, False if the flow is turbulent
+
     def append_operating_conditions(self,segment,energy_conditions,noise_conditions=None): 
         append_motor_conditions(self,segment,energy_conditions)
         return
+    
+
+
     
