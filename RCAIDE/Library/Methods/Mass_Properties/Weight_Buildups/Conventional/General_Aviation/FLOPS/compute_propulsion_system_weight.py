357d5d7dd1570d3e50f89cfef4f50dd68a9465ec
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2025-09-26 10:39:35 -0500

    Update compute_propulsion_system_weight.py
    Updated total weight and corrected nacelle, thrust reverse, starter, and engine control calcualtions

--- RCAIDE/Library/Methods/Mass_Properties/Weight_Buildups/Conventional/General_Aviation/FLOPS/compute_propulsion_system_weight.py
+++ RCAIDE/Library/Methods/Mass_Properties/Weight_Buildups/Conventional/General_Aviation/FLOPS/compute_propulsion_system_weight.py
@@ -101,15 +101,15 @@ def compute_propulsion_system_weight(vehicle,network):
                 if 'nacelle' in propulsor: 
                     if propulsor.nacelle !=  None:                    
                         ref_nacelle =  propulsor.nacelle   
-                        WNAC = compute_nacelle_weight(propulsor,ref_nacelle,JNENG)
-                WTHR = compute_thrust_reverser_weight(propulsor,JNENG)
-                WEC, WSTART = compute_misc_propulsion_system_weight(vehicle,propulsor,ref_nacelle,JNENG )
+                        WNAC += compute_nacelle_weight(propulsor,ref_nacelle,JNENG)
+                WTHR += compute_thrust_reverser_weight(propulsor,JNENG)
+                WEC, WSTART += compute_misc_propulsion_system_weight(vehicle,propulsor,ref_nacelle,JNENG )
     
     NENG = JNENG + PNENG
                   
     WFSYS           = compute_fuel_system_weight(vehicle, NENG)
     
-    WPRO            = WENG + WFSYS
+    WPRO            = WENG + WFSYS + WTHR + WSTART + WEC + WNAC
 
     output                      = Data()
     output.W_prop               = WPRO
