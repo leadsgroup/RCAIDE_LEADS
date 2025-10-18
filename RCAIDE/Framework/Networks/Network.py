19cf926388a0c4c3a2b571ba96bb6c9ce26edd92
Matteo Guidotti <matteog2@illinois.edu>
2025-10-18 16:22:11 -0500

    Refactor TRU performance computation and network power update

--- RCAIDE/Framework/Networks/Network.py
+++ RCAIDE/Framework/Networks/Network.py
@@ -185,7 +185,7 @@ class Network(Component):
             P_mech, P_elec, P_hydr, P_therm, stored_results_flag, stored_modulator_tag = modulator.compute_performance(network, state)
             
             Network.update_distributor_net_power(modulator, network, conditions, P_mech, P_elec, P_hydr, P_therm)  
-        
+       
         # -------------------------------------------------------------------------------------------------------------------
         # Other Distributors 
         # -------------------------------------------------------------------------------------------------------------------
@@ -195,8 +195,8 @@ class Network(Component):
                 
                 P_mech, P_elec, P_hydr, P_therm = network.distributors[distributor_tag[0]].compute_performance(state)
 
-                Network.update_distributor_net_power(network.distributors[distributor_tag[0]], network, conditions, P_mech, P_elec, P_hydr, P_therm)  
-      
+                Network.update_distributor_net_power(network.distributors[distributor_tag[0]], network, conditions, P_mech, - P_elec, P_hydr, P_therm)  
+
         # ----------------------------------------------------------        
         # Sources
         # ----------------------------------------------------------
@@ -234,8 +234,6 @@ class Network(Component):
 
         # if isinstance(distributor,RCAIDE.Library.Components.Powertrain.Distributors.Electrical_Bus):   
         #     total_elec_power        -= state.conditions.energy.distributors[distributor.tag].regenerative_power*bus_voltage* distributor.power_split_ratio  /distributor.efficiency   
-        
-        
           
 
     # # ----------------------------------------------------------
@@ -286,7 +284,7 @@ class Network(Component):
                         conditions.energy.distributors[dist_tag].net_electrical_power  += P_elec * dist.power_split_ratio / dist.electrical_efficiency
                     else:
                         conditions.energy.distributors[dist_tag].net_electrical_power  += - P_elec * component.electrical_efficiency * dist.power_split_ratio / dist.electrical_efficiency
-                else:   
+                else:    
                     conditions.energy.distributors[dist_tag].net_electrical_power  += P_elec * dist.power_split_ratio / dist.electrical_efficiency
             
             # elif isinstance(network.distributors[dist_tag], RCAIDE.Library.Components.Powertrain.Distributors.Mechanical_Line):
--- RCAIDE/Library/Methods/Powertrain/Modulators/Transformer_Rectifier_Unit/compute_tru_performance.py
+++ RCAIDE/Library/Methods/Powertrain/Modulators/Transformer_Rectifier_Unit/compute_tru_performance.py
@@ -10,65 +10,76 @@ def compute_tru_performance(tru, network, state):
     """
    
     """
-    tru_conditions = state.conditions.energy.modulators[tru.tag]
+    conditions     = state.conditions
+    tru_conditions = conditions.energy.modulators[tru.tag]
+
+    # -------------------------------------------------
+    # Read buses (AC power in, AC LL voltage, DC bus V)
+    # -------------------------------------------------
+    P_in = 0.0 * state.ones_row(1)   # [W] AC-side real power drawn
+    Vll  = 0.0                       # [V_rms] AC line-line
+    Vdc  = 0.0                       # [V] DC bus
 
     for distributor_tag in tru.assigned_distributors[0]:
-        if network.distributors[distributor_tag].bus_type == 'AC':
-            Vll = network.distributors[distributor_tag].voltage_phase_to_phase
-        elif network.distributors[distributor_tag].bus_type == 'DC':
-            Vdc = network.distributors[distributor_tag].voltage
+        dist = network.distributors[distributor_tag]
+        if dist.bus_type == 'AC':
+            P_in += conditions.energy.distributors[distributor_tag].net_electrical_power
+            Vll   = dist.voltage_phase_to_phase
+        elif dist.bus_type == 'DC':
+            Vdc   = dist.voltage
 
-    # -------------------------
-    # Component parameters 
-    # -------------------------
+    # -------------------------------------------------
+    # TRU parameters (keep it simple)
+    # -------------------------------------------------
     eta        = tru.electrical_efficiency
     turns      = tru.turns_ratio
     diode_drop = tru.diode_drop
     R_load     = tru.R_load
     Idc_set    = tru.Idc_set
 
-    # -------------------------
-    # Estimate DC voltage if DC bus voltage isn't provided
-    # Vdc_open ≈ 1.35 * Vll_sec - 2*diode_drop  (6-pulse)
-    # -------------------------
+    # -------------------------------------------------
+    # Voltages (estimate DC if no DC bus voltage given)
+    # Vdc_open ≈ 1.35 * (Vll/turns) - 2*diode_drop
+    # -------------------------------------------------
     Vll_sec  = Vll / turns
     Vdc_open = max(1.35 * Vll_sec - 2.0 * diode_drop, 0.0)
-    if Vdc <= 0.0:
-        Vdc_use = Vdc_open
-    else:
+    if Vdc > 0.0:
         Vdc_use = Vdc
-
-    # -------------------------
-    # Choose DC current (Idc)
-    # -------------------------
-    if Idc_set is not None:
-        Idc = float(Idc_set)
-    elif R_load is not None and float(R_load) > 0.0:
-        Idc = Vdc_use / float(R_load)
     else:
-        Idc = 0.0 
+        Vdc_use = Vdc_open
 
-    # -------------------------
-    # Powers
-    # -------------------------
-    P_out = Vdc_use * Idc            # DC side power delivered
-    P_in  = P_out / eta              # AC side real power drawn
+    # -------------------------------------------------
+    # Output power from input power
+    # -------------------------------------------------
+    P_out = eta * P_in                      # [W] DC-side power delivered
 
-    # -------------------------
-    # Write a few useful I/O
-    # -------------------------
-    tru_conditions.inputs.Vll_rms_primary          = Vll
-    tru_conditions.outputs.ac_voltage_secondary    = Vll_sec
-    tru_conditions.outputs.dc_voltage_average      = Vdc_use
-    tru_conditions.outputs.dc_current              = Idc
-    tru_conditions.outputs.dc_real_power           = P_out * state.ones_row(1)
-    tru_conditions.inputs.ac_real_power            = P_in
+    # -------------------------------------------------
+    # DC current choice
+    # -------------------------------------------------
+    if Idc_set is not None:
+        Idc = Idc_set * state.ones_row(1)
+    elif (R_load is not None) and (R_load > 0.0):
+        Idc = Vdc_use / R_load
+    else:
+        # infer from power balance if no load/current specified
+        Idc = P_out / Vdc_use if Vdc_use != 0.0 else 0.0 * state.ones_row(1)
 
-    # -------------------------
-    # Return to network
-    # -------------------------
+    # -------------------------------------------------
+    # Record I/O
+    # -------------------------------------------------
+    tru_conditions.inputs.Vll_rms_primary       = Vll
+    tru_conditions.outputs.ac_voltage_secondary = Vll_sec
+    tru_conditions.outputs.dc_voltage_average   = Vdc_use
+    tru_conditions.outputs.dc_current           = Idc
+    tru_conditions.outputs.dc_real_power        = P_out
+    tru_conditions.inputs.ac_real_power         = P_in
+
+    # -------------------------------------------------
+    # Return powers to the network evaluator
+    # (source on DC side → negative sign for P_elec)
+    # -------------------------------------------------
     P_mech = 0.0 * state.ones_row(1)
-    P_elec = - P_out * state.ones_row(1)
+    P_elec = - P_out
     P_hydr = 0.0 * state.ones_row(1)
     P_therm= 0.0 * state.ones_row(1)
     stored_results_flag  = True
