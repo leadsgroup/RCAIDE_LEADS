af8459f61d966adfc9a8e89cc80a36dbf3924365
Sai Sankalp Shekar <sshekar2@illinois.edu>
2026-03-19 13:48:47 +0530

    updates

--- VnV/Verification/network_turbojet/turbojet_network_test.py
+++ VnV/Verification/network_turbojet/turbojet_network_test.py
@@ -66,7 +66,7 @@ def main():
             print(val)
     
     # Truth values 
-    L_D_truth         = 7.247823909318594
+    L_D_truth         = 6.959145341749945
     
     # Store errors 
     error = Data() 
