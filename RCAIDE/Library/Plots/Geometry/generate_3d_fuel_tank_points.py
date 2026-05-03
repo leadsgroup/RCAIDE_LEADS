ee3cccf7d2530280edf6ce522d78f3eed594f206
Matthew Clarke <mclarke2@stanford.edu>
2026-05-03 03:18:09 -0500

    Update generate_3d_fuel_tank_points.py

--- RCAIDE/Library/Plots/Geometry/generate_3d_fuel_tank_points.py
+++ RCAIDE/Library/Plots/Geometry/generate_3d_fuel_tank_points.py
@@ -76,148 +76,182 @@ def generate_integral_wing_tank_points(wing, n_points, segment_list,fuel_tank):
     if len(segments) > 0: 
         if segment_list[0] == None or  segment_list[1] == None:
             raise Exception('Tank segments must be defined')
-        pts              = np.zeros((2,n_points, 3,1))  
-        section_twist    = np.zeros((2,n_points, 3,3))
-        section_twist[:, :, 0, 0] = 1        
-        section_twist[:, :, 1, 1] = 1
-        section_twist[:, :, 2, 2] = 1 
-        translation        = np.zeros((2,n_points, 3,1))    
-        translation[:, :, 0,:] = origin[0][0]  
-        translation[:, :, 1,:] = origin[0][1]  
-        translation[:, :, 2,:] = origin[0][2]  
-        for i in range(len(segment_list)):
-            current_seg = segments[segment_list[i]]
-            fs = fuel_tank.segments_percent_chord_start
-            rs = fuel_tank.segments_percent_chord_end  
-            front_rib_yu,rear_rib_yu,front_rib_yl,rear_rib_yl = compute_non_dimensional_rib_coordinates(current_seg,fuel_tank,fs[i], rs[i])
-            x_coordinates =  np.array([rs[i], rs[i], fs[i], fs[i], rs[i]])
-            y_coordinates =  np.array([rear_rib_yl, rear_rib_yu, front_rib_yu,front_rib_yl,rear_rib_yl ])   
-            twist    = current_seg.twist 
-            if wing.vertical:  
-                pts[i,:,0,0]   = x_coordinates * current_seg.root_chord_percent * wing.chords.root 
-                pts[i,:,1,0]   = y_coordinates * current_seg.root_chord_percent * wing.chords.root 
-                pts[i,:,2,0]   = np.zeros_like(y_coordinates) 
-              
-                section_twist[i,:,0,0] = np.cos(twist) 
-                section_twist[i,:,0,1] = -np.sin(twist)  
-                section_twist[i,:,1,0] = np.sin(twist) 
-                section_twist[i,:,1,1] = np.cos(twist)  
-            else: 
-                pts[i,:,0,0]   = x_coordinates * current_seg.root_chord_percent * wing.chords.root
-                pts[i,:,1,0]   = np.zeros_like(y_coordinates) 
-                pts[i,:,2,0]   = y_coordinates * current_seg.root_chord_percent * wing.chords.root   
-
-                section_twist[i,:,0,0] = np.cos(twist) 
-                section_twist[i,:,0,2] = np.sin(twist)  
-                section_twist[i,:,2,0] = -np.sin(twist) 
-                section_twist[i,:,2,2] =  np.cos(twist)  
-    
-            translation[i, :, 0,:] += current_seg.origin[0][0]  
-            translation[i, :, 1,:] += current_seg.origin[0][1]  
-            translation[i, :, 2,:] += current_seg.origin[0][2]
-                
-            if (i == n_segments-1):  
-                # update origin for next segment 
-                prev_seg             = segments[segment_list[i-1]]                
-                segment_percent_span = current_seg.percent_span_location -  prev_seg.percent_span_location
-
-                sweep    = prev_seg.sweeps.leading_edge
-                dihedral = prev_seg.dihedral_outboard
-            
-                if wing.vertical:
-                    dz = semispan*segment_percent_span
-                    dy = dz*np.tan(dihedral)
-                    l  = dz/np.cos(dihedral)
-                    dx = l*np.tan(sweep)
-                else:
-                    dy = semispan*segment_percent_span
-                    dz = dy*np.tan(dihedral)
-                    l  = dy/np.cos(dihedral)
-                    dx = l*np.tan(sweep)
-                    
-                translation[i,:,0,:] = translation[i-1,:,0,:] + dx
-                translation[i,:,1,:] = translation[i-1,:,1,:] + dy
-                translation[i,:,2,:] = translation[i-1,:,2,:] + dz 
-    else:
-
-        pts                       = np.zeros((2,n_points, 3,1))  
-        section_twist             = np.zeros((2,n_points, 3,3))
-        section_twist[:, :, 0, 0] = 1        
+        pts              = np.zeros((n_segments + 2, n_points, 3, 1))
+        section_twist    = np.zeros((n_segments + 2, n_points, 3, 3))
+        section_twist[:, :, 0, 0] = 1
         section_twist[:, :, 1, 1] = 1
         section_twist[:, :, 2, 2] = 1
-        translation               = np.zeros((2,n_points, 3,1))
+        translation      = np.zeros((n_segments + 2, n_points, 3, 1))
+        translation[:, :, 0, :] = origin[0][0]
+        translation[:, :, 1, :] = origin[0][1]
+        translation[:, :, 2, :] = origin[0][2]
+
+        fs = fuel_tank.segments_percent_chord_start
+        rs = fuel_tank.segments_percent_chord_end
+
+        for i in range(n_segments + 2):
+            is_end_cap = (i == 0 or i == n_segments + 1)
+
+            if i == 0:
+                seg_idx = 0
+            elif i == n_segments + 1:
+                seg_idx = n_segments - 1
+            else:
+                seg_idx = i - 1
+
+            current_seg = segments[segment_list[seg_idx]]
+            front_rib_yu, rear_rib_yu, front_rib_yl, rear_rib_yl = compute_non_dimensional_rib_coordinates(current_seg, fuel_tank, fs[seg_idx], rs[seg_idx])
+            x_coordinates = np.array([rs[seg_idx], rs[seg_idx], fs[seg_idx], fs[seg_idx], rs[seg_idx]])
+            y_coordinates = np.array([rear_rib_yl, rear_rib_yu, front_rib_yu, front_rib_yl, rear_rib_yl])
+            twist = current_seg.twist
+
+            if wing.vertical:
+                pts[i, :, 0, 0] = x_coordinates * current_seg.root_chord_percent * wing.chords.root
+                pts[i, :, 1, 0] = (y_coordinates + y_coordinates[::-1]) / 2 if is_end_cap else y_coordinates * current_seg.root_chord_percent * wing.chords.root
+                pts[i, :, 2, 0] = np.zeros_like(y_coordinates)
+
+                section_twist[i, :, 0, 0] = np.cos(twist)
+                section_twist[i, :, 0, 1] = -np.sin(twist)
+                section_twist[i, :, 1, 0] = np.sin(twist)
+                section_twist[i, :, 1, 1] = np.cos(twist)
+            else:
+                pts[i, :, 0, 0] = x_coordinates * current_seg.root_chord_percent * wing.chords.root
+                pts[i, :, 1, 0] = np.zeros_like(y_coordinates)
+                pts[i, :, 2, 0] = (y_coordinates + y_coordinates[::-1]) / 2 if is_end_cap else y_coordinates * current_seg.root_chord_percent * wing.chords.root
+
+                section_twist[i, :, 0, 0] = np.cos(twist)
+                section_twist[i, :, 0, 2] = np.sin(twist)
+                section_twist[i, :, 2, 0] = -np.sin(twist)
+                section_twist[i, :, 2, 2] = np.cos(twist)
+
+            if i != n_segments + 1:
+                translation[i, :, 0, :] += current_seg.origin[0][0]
+                translation[i, :, 1, :] += current_seg.origin[0][1]
+                translation[i, :, 2, :] += current_seg.origin[0][2]
+
+            if i == n_segments:
+                prev_seg             = segments[segment_list[seg_idx - 1]]
+                segment_percent_span = current_seg.percent_span_location - prev_seg.percent_span_location
+                sweep                = prev_seg.sweeps.leading_edge
+                dihedral             = prev_seg.dihedral_outboard
+
+                if wing.vertical:
+                    dz = semispan * segment_percent_span
+                    dy = dz * np.tan(dihedral)
+                    l  = dz / np.cos(dihedral)
+                    dx = l  * np.tan(sweep)
+                else:
+                    dy = semispan * segment_percent_span
+                    dz = dy * np.tan(dihedral)
+                    l  = dy / np.cos(dihedral)
+                    dx = l  * np.tan(sweep)
+
+                translation[i, :, 0, :] = translation[i - 1, :, 0, :] + dx
+                translation[i, :, 1, :] = translation[i - 1, :, 1, :] + dy
+                translation[i, :, 2, :] = translation[i - 1, :, 2, :] + dz
+            elif i == n_segments + 1:
+                translation[i, :, :, :] = translation[i - 1, :, :, :]
+
+    else:
+
+        pts           = np.zeros((4, n_points, 3, 1))
+        section_twist = np.zeros((4, n_points, 3, 3))
+        section_twist[:, :, 0, 0] = 1
+        section_twist[:, :, 1, 1] = 1
+        section_twist[:, :, 2, 2] = 1
+        translation   = np.zeros((4, n_points, 3, 1))
 
         fs                = fuel_tank.segments_percent_chord_start
-        rs                = fuel_tank.segments_percent_chord_end       
-        front_rib_yu_i,rear_rib_yu_i,front_rib_yl_i,rear_rib_yl_i = compute_non_dimensional_rib_coordinates(wing,fuel_tank,fs[0],rs[0])
-        front_rib_yu_o,rear_rib_yu_o,front_rib_yl_o,rear_rib_yl_o = compute_non_dimensional_rib_coordinates(wing,fuel_tank,fs[1],rs[1])
-        x_coordinates_i   =  np.array([rs[0], rs[0], fs[0], fs[0], rs[0]])
-        x_coordinates_o   =  np.array([rs[1], rs[1], fs[1], fs[1], rs[1]])
-        y_coordinates_i   =  np.array([rear_rib_yl_i, rear_rib_yu_i, front_rib_yu_i,front_rib_yl_i,rear_rib_yl_i ]) 
-        y_coordinates_o   =  np.array([rear_rib_yl_o, rear_rib_yu_o, front_rib_yu_o,front_rib_yl_o,rear_rib_yl_o ]) 
-            
-        dihedral              = wing.dihedral
-        if wing.sweeps.leading_edge  is not None: 
-            sweep      = wing.sweeps.leading_edge
-        else:  
-            sweep_quarter_chord = wing.sweeps.quarter_chord 
-            chord_fraction      = 0.25                          
+        rs                = fuel_tank.segments_percent_chord_end
+        front_rib_yu_i, rear_rib_yu_i, front_rib_yl_i, rear_rib_yl_i = compute_non_dimensional_rib_coordinates(wing, fuel_tank, fs[0], rs[0])
+        front_rib_yu_o, rear_rib_yu_o, front_rib_yl_o, rear_rib_yl_o = compute_non_dimensional_rib_coordinates(wing, fuel_tank, fs[1], rs[1])
+        x_coordinates_i = np.array([rs[0], rs[0], fs[0], fs[0], rs[0]])
+        x_coordinates_o = np.array([rs[1], rs[1], fs[1], fs[1], rs[1]])
+        y_coordinates_i = np.array([rear_rib_yl_i, rear_rib_yu_i, front_rib_yu_i, front_rib_yl_i, rear_rib_yl_i])
+        y_coordinates_o = np.array([rear_rib_yl_o, rear_rib_yu_o, front_rib_yu_o, front_rib_yl_o, rear_rib_yl_o])
+
+        dihedral = wing.dihedral
+        if wing.sweeps.leading_edge is not None:
+            sweep = wing.sweeps.leading_edge
+        else:
+            sweep_quarter_chord = wing.sweeps.quarter_chord
+            chord_fraction      = 0.25
             segment_root_chord  = wing.chords.root
             segment_tip_chord   = wing.chords.tip
-            segment_span        = semispan 
-            sweep       = np.arctan(((segment_root_chord*chord_fraction) + (np.tan(sweep_quarter_chord )*segment_span - chord_fraction*segment_tip_chord)) /segment_span)  
-           
-        # append root section     
-        translation[:, :, 0,:] = origin[0][0]  
-        translation[:, :, 1,:] = origin[0][1]  
-        translation[:, :, 2,:] = origin[0][2] 
-       
-        if wing.vertical: 
-            pts[0,:,0,0]   = x_coordinates_i *  wing.chords.root
-            pts[0,:,1,0]   = y_coordinates_i *  wing.chords.root
-            pts[0,:,2,0]   = np.zeros_like(y_coordinates_i)
-            
-            pts[1,:,0,0]   = x_coordinates_o *  wing.chords.tip  
-            pts[1,:,1,0]   = y_coordinates_o *  wing.chords.tip  
-            pts[1,:,2,0]   = np.zeros_like(y_coordinates_o)   
-            
-            translation[1, :, 0,:] += semispan*np.tan(sweep)
-            translation[1, :, 1,:] += semispan*np.tan(dihedral) 
-            translation[1, :, 2,:] += semispan 
+            segment_span        = semispan
+            sweep = np.arctan(((segment_root_chord * chord_fraction) + (np.tan(sweep_quarter_chord) * segment_span - chord_fraction * segment_tip_chord)) / segment_span)
 
-            section_twist[0,:,0,0] = np.cos(wing.twists.root) 
-            section_twist[0,:,0,1] = -np.sin(wing.twists.root)  
-            section_twist[0,:,1,0] = np.sin(wing.twists.root) 
-            section_twist[0,:,1,1] = np.cos(wing.twists.root)
-             
-            section_twist[1,:,0,0] = np.cos(wing.twists.tip) 
-            section_twist[1,:,0,1] = -np.sin(wing.twists.tip)  
-            section_twist[1,:,1,0] = np.sin(wing.twists.tip) 
-            section_twist[1,:,1,1] = np.cos(wing.twists.tip)
-            
-            
+        translation[:, :, 0, :] = origin[0][0]
+        translation[:, :, 1, :] = origin[0][1]
+        translation[:, :, 2, :] = origin[0][2]
+
+        if wing.vertical:
+            # root (slot 1) and tip (slot 2); slots 0 and 3 are end caps
+            pts[1, :, 0, 0] = x_coordinates_i * wing.chords.root
+            pts[1, :, 1, 0] = y_coordinates_i * wing.chords.root
+            pts[1, :, 2, 0] = np.zeros_like(y_coordinates_i)
+
+            pts[2, :, 0, 0] = x_coordinates_o * wing.chords.tip
+            pts[2, :, 1, 0] = y_coordinates_o * wing.chords.tip
+            pts[2, :, 2, 0] = np.zeros_like(y_coordinates_o)
+
+            translation[2, :, 0, :] += semispan * np.tan(sweep)
+            translation[2, :, 1, :] += semispan * np.tan(dihedral)
+            translation[2, :, 2, :] += semispan
+
+            section_twist[1, :, 0, 0] = np.cos(wing.twists.root)
+            section_twist[1, :, 0, 1] = -np.sin(wing.twists.root)
+            section_twist[1, :, 1, 0] = np.sin(wing.twists.root)
+            section_twist[1, :, 1, 1] = np.cos(wing.twists.root)
+
+            section_twist[2, :, 0, 0] = np.cos(wing.twists.tip)
+            section_twist[2, :, 0, 1] = -np.sin(wing.twists.tip)
+            section_twist[2, :, 1, 0] = np.sin(wing.twists.tip)
+            section_twist[2, :, 1, 1] = np.cos(wing.twists.tip)
+
+            # end caps: same x/z, profile collapsed to midline in y
+            pts[0, :, 0, 0] = pts[1, :, 0, 0]
+            pts[0, :, 1, 0] = (pts[1, :, 1, 0] + pts[1, :, 1, 0][::-1]) / 2
+            pts[0, :, 2, 0] = pts[1, :, 2, 0]
+            pts[3, :, 0, 0] = pts[2, :, 0, 0]
+            pts[3, :, 1, 0] = (pts[2, :, 1, 0] + pts[2, :, 1, 0][::-1]) / 2
+            pts[3, :, 2, 0] = pts[2, :, 2, 0]
+
         else:
-            pts[0,:,0,0]   = x_coordinates_i *  wing.chords.root
-            pts[0,:,1,0]   = np.zeros_like(y_coordinates_i) 
-            pts[0,:,2,0]   = y_coordinates_i *  wing.chords.root
-            
-            pts[1,:,0,0]   = x_coordinates_o *  wing.chords.tip  
-            pts[1,:,1,0]   = np.zeros_like(y_coordinates_o)  
-            pts[1,:,2,0]   = y_coordinates_o *  wing.chords.tip   
-    
-            translation[1, :, 0,:] += semispan*np.tan(sweep)
-            translation[1, :, 1,:] += semispan 
-            translation[1, :, 2,:] += semispan*np.tan(dihedral)     
+            pts[1, :, 0, 0] = x_coordinates_i * wing.chords.root
+            pts[1, :, 1, 0] = np.zeros_like(y_coordinates_i)
+            pts[1, :, 2, 0] = y_coordinates_i * wing.chords.root
 
-            section_twist[0,:,0,0] = np.cos(wing.twists.root) 
-            section_twist[0,:,0,2] = np.sin(wing.twists.root)  
-            section_twist[0,:,2,0] = -np.sin(wing.twists.root) 
-            section_twist[0,:,2,2] =  np.cos(wing.twists.root)
-             
-            section_twist[1,:,0,0] = np.cos(wing.twists.tip) 
-            section_twist[1,:,0,2] = np.sin(wing.twists.tip)  
-            section_twist[1,:,2,0] = -np.sin(wing.twists.tip) 
-            section_twist[1,:,2,2] =  np.cos(wing.twists.tip) 
+            pts[2, :, 0, 0] = x_coordinates_o * wing.chords.tip
+            pts[2, :, 1, 0] = np.zeros_like(y_coordinates_o)
+            pts[2, :, 2, 0] = y_coordinates_o * wing.chords.tip
+
+            translation[2, :, 0, :] += semispan * np.tan(sweep)
+            translation[2, :, 1, :] += semispan
+            translation[2, :, 2, :] += semispan * np.tan(dihedral)
+
+            section_twist[1, :, 0, 0] = np.cos(wing.twists.root)
+            section_twist[1, :, 0, 2] = np.sin(wing.twists.root)
+            section_twist[1, :, 2, 0] = -np.sin(wing.twists.root)
+            section_twist[1, :, 2, 2] = np.cos(wing.twists.root)
+
+            section_twist[2, :, 0, 0] = np.cos(wing.twists.tip)
+            section_twist[2, :, 0, 2] = np.sin(wing.twists.tip)
+            section_twist[2, :, 2, 0] = -np.sin(wing.twists.tip)
+            section_twist[2, :, 2, 2] = np.cos(wing.twists.tip)
+
+            # end caps: same x/y, profile collapsed to midline in z
+            pts[0, :, 0, 0] = pts[1, :, 0, 0]
+            pts[0, :, 1, 0] = pts[1, :, 1, 0]
+            pts[0, :, 2, 0] = (pts[1, :, 2, 0] + pts[1, :, 2, 0][::-1]) / 2
+            pts[3, :, 0, 0] = pts[2, :, 0, 0]
+            pts[3, :, 1, 0] = pts[2, :, 1, 0]
+            pts[3, :, 2, 0] = (pts[2, :, 2, 0] + pts[2, :, 2, 0][::-1]) / 2
+
+        # end cap twists and translations match their adjacent cross-section
+        section_twist[0] = section_twist[1]
+        section_twist[3] = section_twist[2]
+        translation[3, :, :, :] = translation[2, :, :, :]
  
     mat     = translation + np.matmul(section_twist ,pts)
     
