050bd9f5101b9bd65a3fafaac76ebf233f238e46
Matthew Clarke <mclarke2@stanford.edu>
2026-06-16 11:34:18 -0500

    Revert "Disable interactive PyVista rendering in CI for 3D vehicle plots"
    This reverts commit 730f97b38ad25554d519116a2369b7d4fd4e10aa.

--- RCAIDE/Library/Plots/Geometry/plot_3d_vehicle.py
+++ RCAIDE/Library/Plots/Geometry/plot_3d_vehicle.py
@@ -25,7 +25,6 @@ from RCAIDE.Library.Methods.Geometry.LOPA                       import compute_l
 # python imports 
 import numpy as np
 from copy import deepcopy
-import os
 import pyvista as pv
 import matplotlib.colors as mcolors
 
@@ -133,12 +132,10 @@ def plot_3d_vehicle(vehicle,
     # -------------------------------------------------------------------------  
     # Initalize Renderer
     # ------------------------------------------------------------------------- 
-    is_ci = os.environ.get("CI", "").lower() == "true"
-    pyvista_off_screen = os.environ.get("PYVISTA_OFF_SCREEN", "").lower() in ("1", "true", "yes")
-    if is_ci or pyvista_off_screen:
-        show_figure = False
-
-    plotter = pv.Plotter(off_screen=(save_figure or not show_figure))
+    if save_figure: 
+        plotter = pv.Plotter(off_screen=True)
+    else:
+        plotter = pv.Plotter()     
     
     # -------------------------------------------------------------------------
     # Object RGB Colors  
