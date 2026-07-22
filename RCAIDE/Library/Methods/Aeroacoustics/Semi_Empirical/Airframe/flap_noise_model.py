fixed distance units and update flap constants

RCAIDE/Library/Methods/Aeroacoustics/Semi_Empirical/Airframe/flap_noise_model.py
@@ -20,14 +20,14 @@ def flap_noise_model(microphone_locations,cf,thickness, deltaf, theta, frequency
         h = thickness,                  # Flap thickness (m)
         L_f = cf,                       # Flap chord length (m)
         alpha = segment.state.conditions.aerodynamics.angles.alpha,                          # Angle of attack (rad)
-        sigma_f = deltaf+segment.state.conditions.aerodynamics.angles.alpha,                 # Flap sweep angle (rad)
+        sigma_f = 0.436332,                 # Flap sweep angle (rad) TO CODE
         gamma_f = deltaf,                                                              # Flap deployment angle (rad)
         M = segment.state.conditions.freestream.mach_number,                                 # Flight Mach number
         U = segment.state.conditions.freestream.velocity,                                    # Flight velocity (m/s)
         c0 = segment.state.conditions.freestream.speed_of_sound,                             # Speed of sound (m/s)
         rho0 = segment.state.conditions.freestream.density,                                  # Ambient density (kg/m^3)
-        r = 121, #np.linalg.norm(microphone_locations,axis = 1),                             # Observer distance (m)
-        theta = 90 #theta/Units.degree,                                                    # Polar angle (overhead = 90 deg) 
+        r = np.linalg.norm(microphone_locations,axis = 1)[0],                             # Observer distance (m)
+        theta = 0 #theta/Units.degree,                                                    # Polar angle (overhead = 90 deg) 
     )
 
     print('FLAP', params)
@@ -54,7 +54,7 @@ def flap_noise_model(microphone_locations,cf,thickness, deltaf, theta, frequency
         'theta': Polar angle (overhead = 90 deg)
     """
     constants = Data(
-        A0= 3, mu0= 7.693, mu1= 10, mu2= 0.292, alpha_0= 0.005
+        A0= 3e-5, mu0= 0.7693, mu1= 1.0, mu2= 0.292, alpha_0= 0.01
     )
         
     p_ref = 2e-5 # Reference SPL in Pascals
@@ -87,9 +87,8 @@ def flap_noise_model(microphone_locations,cf,thickness, deltaf, theta, frequency
         A_G = calc_geometric_amplitude(params, is_high_freq, constants.A0)
         A_F = 1.0 # Standard assumption if flow is bundled into A0
         W_M = (M**n) / I_M
-        for is_high_freq in [False, True]:
             # Calculate fresh for this band
-            f_source = (frequency * Delta) + (10 if not is_high_freq else 100)
+        f_source = (frequency / Delta)
         F_f = calc_spectral_shape(f_source, M, l, c0, U, 
                                 constants.mu0, constants.mu1, constants.mu2)
 
@@ -104,7 +103,7 @@ def flap_noise_model(microphone_locations,cf,thickness, deltaf, theta, frequency
         component_SPL = 10.0 * np.log10(PSD_component / (p_ref**2))
         comp_li.append(component_SPL)
     # Convert total PSD [Pa^2/Hz] to SPL [dB/Hz]
-    SPL = 10.0 * np.log10(PSD_total / (p_ref**2))
+    SPL = 10.0 * (np.log10(PSD_total / (p_ref**2)) + np.log10(0.231 * frequency))
     return SPL #constant added
 
 

RCAIDE/Library/Methods/Aeroacoustics/Semi_Empirical/Airframe/landing_gear_noise_model.py
@@ -59,10 +59,10 @@ def compute_landing_gear_noise(microphone_locations, D, H, W, wheels, M, Weight,
 
     gear_params = Data(
         num_wheels = wheels,
-        wheel_diam = D / Units.ft,                  # Approximate (in)
-        wheel_width = W / Units.ft,                 # Approximate (in)
-        strut_lengths = [H / Units.ft],             # Total length L=317 in
-        strut_dims = [strut_diameter / Units.ft],   # Average dimension a=4.65 in
+        wheel_diam = D / Units.inches,                  # Approximate (in)
+        wheel_width = W / Units.inches,                 # Approximate (in)
+        strut_lengths = [H / Units.inches],             # Total length L=317 in
+        strut_dims = [strut_diameter / Units.inches],   # Average dimension a=4.65 in
         aircraft_weight = Weight / Units.pounds,        # Reference weight (lbs)
         track_angle = 0.0,                              # Assume track angle of zero
     )
@@ -70,16 +70,18 @@ def compute_landing_gear_noise(microphone_locations, D, H, W, wheels, M, Weight,
 
     flight_cond = Data(
         M_flight = M / 0.75, 
-        theta = theta / Units.rad, # (deg),
-        R = 10, #np.linalg.norm(microphone_locations, axis=1) if hasattr(microphone_locations, 'ndim') else microphone_locations,
+        theta = theta / Units.deg, # (deg),
+        R = np.linalg.norm(microphone_locations, axis=1)[0]/Units.feet, #if hasattr(microphone_locations, 'ndim') else microphone_locations,
         c0 = (segment.state.conditions.freestream.speed_of_sound / Units.foot_per_second), # sound speed (ft/s)
         rho0 = segment.state.conditions.freestream.density / Units["slugs/ft^3"]   # slug/ft^3
     )
+
+    print("GEAR", gear_params, flight_cond)
     return predict_spectrum(frequency, gear_params, flight_cond)
 
 
 # --- Global Constants & Empirical Parameters ---
-P_REF_VAL = 4.177e-7  # Reference pressure [psf]
+P_REF_VAL = (2*10**-5)/Units.psf  # Reference pressure [psf]
 
 NOISE_PARAMS = Data(
     Low = Data(
@@ -100,11 +102,11 @@ NOISE_PARAMS = Data(
 
 def calculate_geometry(gear_params):
     """Calculates S (Area), l0 (Length scale), and complexity factor for each regime."""
-    Nw = gear_params.num_wheels
-    w_ft = gear_params.wheel_width
-    d_ft = gear_params.wheel_diam
-    L_struts_ft = np.array(gear_params.strut_lengths)
-    D_struts_ft = np.array(gear_params.strut_dims)
+    Nw = gear_params.num_wheels 
+    w_ft = gear_params.wheel_width /12
+    d_ft = gear_params.wheel_diam /12
+    L_struts_ft = np.array(gear_params.strut_lengths) /12
+    D_struts_ft = np.array(gear_params.strut_dims) /12
     W_ac = gear_params.aircraft_weight
     gamma = np.radians(gear_params.track_angle)
     

RCAIDE/Library/Methods/Aeroacoustics/Semi_Empirical/Airframe/slat_noise_model.py
@@ -61,7 +61,7 @@ def slat_noise(microphone_locations, phi, theta, Ls, gamma_s, sigma_s, alpha, se
     """
     #Unpack Segment Data:
     M = segment.state.conditions.freestream.mach_number
-    distance = 100 #ft np.linalg.norm(microphone_locations,axis = 1)
+    distance = np.linalg.norm(microphone_locations,axis = 1)[0]/ Units.feet
     rho_0 = segment.state.conditions.freestream.density / Units["slugs/ft^3"]
     c_0 = segment.state.conditions.freestream.speed_of_sound / Units["ft/s"]
     velocity = segment.state.conditions.freestream.velocity / Units["ft/s"]
@@ -105,14 +105,13 @@ def slat_noise(microphone_locations, phi, theta, Ls, gamma_s, sigma_s, alpha, se
     
     # Overall Scaling Factor
     ambient_scale = (rho_0 * c_0**2)**2
-    print(distance)
     spherical_spreading = 1/(distance**2)
     
     # Assemble Far-Field Noise Power Spectral Density (Pi)
     Pi = A * ambient_scale * W_M * spherical_spreading * convective_amplification * D_theta_phi * F_St
     
     # Convert to Sound Pressure Level (dB)
-    p_ref_psf = 1/Units.psf #reference lowest spl
-    SPL = 10.0 * np.log10(Pi / (p_ref_psf**2) + 1e-12)
+    p_ref_psf = (2*10**-5)/Units.psf #reference lowest spl
+    SPL = 10.0 * np.log10(Pi / (p_ref_psf**2) + 1e-12) + + np.log10(0.231 * frequency)
     
     return SPL

RCAIDE/Library/Methods/Aeroacoustics/Semi_Empirical/Propulsion/Engine_Noise/compute_core_noise.py
@@ -59,7 +59,7 @@ def compute_core_noise(microphone_locations, turbofan, pr, aeroacoustic_data, se
 
     frequency              = frequencies    
     n_cpts                 = len(noise_time)     
-    n_freq                  = len(frequency) 
+    n_freq                 = len(frequency) 
     n_mic                  = len(microphone_locations)
   
     # ============================================================================= 
@@ -141,13 +141,13 @@ def compute_core_noise(microphone_locations, turbofan, pr, aeroacoustic_data, se
     for i in range(n_mic): #to vectorize next
     
         model_inputs = Data(
-        W1 = ((Area_secondary*Velocity_secondary*density_secondary) / Units.lbm),  # Total core mass flow rate (lbm/sec)
+        W1 = ((Area_primary*Velocity_primary*density_primary) / Units.lbm),  # Total core mass flow rate (lbm/sec)
         T_C_o = segment.state.conditions.energy.converters['combustor'].outputs.static_temperature * 1.8,             # Combustor outlet total temperature (deg R)
         T_C_i = segment.state.conditions.energy.converters['combustor'].inputs.static_temperature * 1.8,              # Combustor inlet total temperature (deg R)
         P_amb = pressure_amb / Units.psi,                                               # Ambient pressure (pa -> psia)
         T_amb = (temp_amb*1.8)[0][0],                                                           # Ambient temperature (deg R)
         n_f =  Num_nozzle,                                                              # Number of fuel nozzles
-        R = 100, #distance_microphone[i] / Units.feet,                                        # Microphone distance (ft)
+        R = np.linalg.norm(microphone_locations,axis=1)[0]/Units.feet, #distance_microphone[i] / Units.feet, # Microphone distance (ft)
         D_h_1 = core_nozzle.diameter / Units.feet,                                      # core nozzle hydraulic diameter
         c_amb = (sound_ambient / Units.feet)[0][0],                                             # Ambient sonic velocity (ft/sec)
         D_C = combustor.diameter / Units.feet,                                          # Combustor diameter (ft)
@@ -194,7 +194,7 @@ def compute_core_noise(microphone_locations, turbofan, pr, aeroacoustic_data, se
         SPL_dBA[:,i]                  = SPL_arithmetic(np.atleast_2d(A_weighting_metric(SPL_total,frequency)),sum_axis=1)
 
     core_noise                   = Data()   
-    core_noise.SPL_1_3_spectrum  = SPL_1_3_spectrum_dBA
+    core_noise.SPL_1_3_spectrum  = SPL_1_3_spectrum
     core_noise.SPL               = SPL
     core_noise.SPL_dBA           = SPL_dBA
 

RCAIDE/Library/Methods/Aeroacoustics/Semi_Empirical/Propulsion/Engine_Noise/compute_fan_noise.py
@@ -38,7 +38,7 @@ def compute_fan_noise(microphone_locations, turbofan, m, aeroacoustic_data, segm
     Pressure_secondary     = aeroacoustic_data.propulsors[turbofan.tag].fan.exit_stagnation_pressure 
     Velocity_aircraft      = segment.state.conditions.freestream.velocity
     noise_time             = segment.state.conditions.frames.inertial.time  
-    distance_microphone    = [74] #np.linalg.norm(microphone_locations,axis = 1)    
+    distance_microphone    = [np.linalg.norm(microphone_locations,axis = 1)[0]]#ft #np.linalg.norm(microphone_locations,axis = 1)    
     Diameter_secondary     = aeroacoustic_data.propulsors[turbofan.tag].fan.diameter
     Num_blades             = aeroacoustic_data.propulsors[turbofan.tag].fan.number_of_blades
 
@@ -106,7 +106,7 @@ def compute_fan_noise(microphone_locations, turbofan, m, aeroacoustic_data, segm
     B_Number = Num_blades,                                             # Number of rotor blades
     inlet_distortion = False                                           # Boolean
     )
-
+    print('FAN',fan_inputs)
     def calc_base_level(inputs):
     #Calculates the mass flow and temperature rise base terms.
         m_0 = 1.0     # 1 lb/sec reference
@@ -230,18 +230,18 @@ def compute_fan_noise(microphone_locations, turbofan, m, aeroacoustic_data, segm
         theta_S = np.tile(theta[None,:],(n_cpts,1))  
         theta_s = np.tile(np.atleast_2d(abs(theta_S[:,i])).T,(1,n_freq))
         spl = calc_combination_tones(fan_inputs, frequency, theta=theta_s)
-        distance_attenuated_spl = spl - 20*np.log10((7.44*0.3048)/distance_microphone[i]) # 7.44ft as the microphone sideline to fan distance
-        spl_values.append(distance_attenuated_spl)
+        distance_attenuated_spl = spl + 20*np.log10(1/distance_microphone[i]) # 1 [m] as the microphone sideline to fan distance
+        
 
-        SPL_1_3_spectrum[:,i,:]       = spl 
-        SPL[:,i]                      = SPL_arithmetic(spl,sum_axis=1 )
-        SPL_1_3_spectrum_dBA[:,i,:]   = A_weighting_metric(spl,frequency)
-        SPL_dBA[:,i]                  = SPL_arithmetic(np.atleast_2d(A_weighting_metric(spl,frequency)),sum_axis=1)
+        SPL_1_3_spectrum[:,i,:]       = distance_attenuated_spl 
+        SPL[:,i]                      = SPL_arithmetic(distance_attenuated_spl,sum_axis=1 )
+        SPL_1_3_spectrum_dBA[:,i,:]   = A_weighting_metric(distance_attenuated_spl,frequency)
+        SPL_dBA[:,i]                  = SPL_arithmetic(np.atleast_2d(A_weighting_metric(distance_attenuated_spl,frequency)),sum_axis=1)
 
 
     
 
-    fan_noise.SPL_1_3_spectrum  = SPL_1_3_spectrum_dBA
+    fan_noise.SPL_1_3_spectrum  = SPL_1_3_spectrum
     fan_noise.SPL               = SPL
     fan_noise.SPL_dBA           = SPL_dBA
     return fan_noise

VnV/Verification/analysis_aeroacoustics/empirical_jet_noise_test.py
@@ -99,7 +99,7 @@ flight_params: dict
     Ls = 0.08128
     gamma_s = np.radians(20)
     sigma_s =  np.radians(25)
-    alpha =  np.radians(30)
+    alpha =  np.radians(10)
 
     #define param for core noise model
     pr = 13.1
@@ -167,11 +167,12 @@ flight_params: dict
 
     # define microphone locations
     microphone_locations = np.zeros((1,3))   
+    microphone_locations = np.array([[80,80,80]])
+    print('setting distance:', np.linalg.norm(microphone_locations, axis=1))
 
     # define segment 
     segment                                                = Segment()  
-    conditions                                             = Results() 
-    conditions.aeroacoustics.relative_microphone_locations = np.repeat(microphone_locations[ np.newaxis,:,: ],1,axis=0)   
+    conditions                                             = Results()  
     conditions.aerodynamics.angles.alpha                   = alpha
     conditions.freestream.density                          = np.ones((ctrl_pts,1)) * density
     conditions.freestream.dynamic_viscosity                = np.ones((ctrl_pts,1)) * dynamic_viscosity   
@@ -289,8 +290,9 @@ flight_params: dict
     # plt.legend()
     # plt.show()
 
+
     fan_noise1 = compute_fan_noise(microphone_locations, turbofan,m, segment.state.conditions.aeroacoustics, segment, frequency)
-    validation_fan = read_noise_data('/Users/siripunn/Desktop/LEADS_WORK/RESEARCH/05_Aeroacoustics/Boeing_Method/Fan Noise/b737_fan_noise_data.csv')
+    validation_fan = read_noise_data('/Users/siripunn/Desktop/LEADS_WORK/RESEARCH/05_Aeroacoustics/Boeing_Method/Fan Noise/b737_fan_noise_data.csv/b737_fan_noise_data.csv')
     core_noise1 = compute_core_noise(microphone_locations, turbofan, pr, segment.state.conditions.aeroacoustics, segment, frequency)
     validation_core = read_noise_data('/Users/siripunn/Desktop/LEADS_WORK/RESEARCH/05_Aeroacoustics/Boeing_Method/Core Noise/b737_core_noise_data.csv')
     # print(core_noise1.SPL_1_3_spectrum)
@@ -299,16 +301,15 @@ flight_params: dict
     # ax.set_xscale('log')
 
     #flap slat noise is current neg. db
-    SPL_total = np.concatenate((lg_noise1.Total,-flap_noise1[0],-slat_noise1[0],fan_noise1.SPL_1_3_spectrum[0][0],core_noise1.SPL_1_3_spectrum[0][0]), axis=0)
+    SPL_total = np.concatenate((lg_noise1.Total,flap_noise1[0],slat_noise1[0],fan_noise1.SPL_1_3_spectrum[0][0],core_noise1.SPL_1_3_spectrum[0][0]), axis=0)
     twodim = np.atleast_2d(SPL_total)
-    total_SPL_dBA = SPL_arithmetic(np.array([lg_noise1.Total,flap_noise1[0],slat_noise1[0],fan_noise1.SPL_1_3_spectrum[0][0],core_noise1.SPL_1_3_spectrum[0][0]]),sum_axis = 0)
-    print(total_SPL_dBA,-slat_noise1[0])
+    total_SPL_classic = SPL_arithmetic(np.array([lg_noise1.Total,flap_noise1[0],slat_noise1[0],fan_noise1.SPL_1_3_spectrum[0][0],core_noise1.SPL_1_3_spectrum[0][0]]),sum_axis = 0)
     fig, ax = plt.subplots(figsize=(8, 5))
     #ax.plot(validation_lg[0],validation_lg[1], 'bo')
-    ax.plot(validation_flap[0],validation_flap[1],'bo')
-    ax.plot(validation_fan[0],validation_fan[1],'bo')
+    #ax.plot(validation_flap[0],validation_flap[1],'bo')
+    #ax.plot(validation_fan[0],validation_fan[1],'bo')
     #ax.plot(validation_core[0],validation_core[1],'bo')
-    ax.plot(frequency,total_SPL_dBA,label='Total Noise')
+    ax.plot(frequency,total_SPL_classic,label='Total Noise')
     ax.plot(frequency,core_noise1.SPL_1_3_spectrum[0][0],label='Core Noise')
     ax.plot(frequency,fan_noise1.SPL_1_3_spectrum[0][0],label='Fan Noise')
     ax.plot(frequency,slat_noise1[0],label='Slat Noise')