c150f65ed9bca8b98a3130c7b008bd2921d0119b
denriquezfirestorm <daniel.enriquez@launchfirestorm.com>
2026-07-27 16:22:02 -0700

    Update compute_dynamic_flight_modes.py
    handle no aileron case

--- RCAIDE/Library/Methods/Stability/Common/compute_dynamic_flight_modes.py
+++ RCAIDE/Library/Methods/Stability/Common/compute_dynamic_flight_modes.py
@@ -188,11 +188,18 @@ def compute_dynamic_flight_modes(state,settings,vehicle):
         Nr = 0.25 * rho * u0 * b_ref**2 * S_ref * SSD.CN_r
         
         # Aileron effectiveness  
-        # WHat is we don't have ailerons? Will this work? Check this
-        ail = conditions.control_surfaces.aileron.static_stability.coefficients                  
-        Ya = 0.5 * rho * u0 * u0 * S_ref * ail.Y 
-        La = 0.5 * rho * u0 * u0 * S_ref * b_ref * ail.L 
-        Na = 0.5 * rho * u0 * u0 * S_ref * b_ref * ail.N 
+        if 'aileron' in conditions.control_surfaces:
+            ail = conditions.control_surfaces.aileron.static_stability.coefficients
+            Ya  = 0.5 * rho * u0 * u0 * S_ref * ail.Y
+            La  = 0.5 * rho * u0 * u0 * S_ref * b_ref * ail.L
+            Na  = 0.5 * rho * u0 * u0 * S_ref * b_ref * ail.N
+        else:
+            import warnings
+            warnings.warn(
+                "No aileron found in control_surfaces; "
+                "setting aileron effectiveness terms (Ya, La, Na) to zero."
+            )
+            Ya = La = Na = 0.0 * u0
         
         BLat[:,0,0] = (Ya / m).T[0]
         BLat[:,1,0] = (La / Ixp + Ixzp * Na).T[0]
