dfaa6153b90c162cff36096db74c215ffb0340a1
Matthew Clarke <mclarke2@stanford.edu>
2026-09-25 17:17:20 -0400

    Update digital_elevation_test.py

--- VnV/Verification/analysis_aeroacoustics/digital_elevation_test.py
+++ VnV/Verification/analysis_aeroacoustics/digital_elevation_test.py
@@ -65,7 +65,7 @@ def main():
     plot_battery_pack_conditions(results) 
 
     X57_SPL        = np.max(results.segments.cruise.conditions.aeroacoustics.SPL_dBA) 
-    X57_SPL_true   = 85.63436752934628
+    X57_SPL_true   = 79.80373369245092
     X57_diff_SPL   = np.abs(X57_SPL - X57_SPL_true)
     print('Error: ',X57_diff_SPL)
     assert np.abs((X57_SPL - X57_SPL_true)/X57_SPL_true) < 1e-3 
